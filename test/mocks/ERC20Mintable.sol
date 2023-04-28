e579a97e82e2fe048ec12fe1565f8a1103d8fecc
Lewis <lewis.broadhurst@mahimarkets.com>
2023-04-28 22:19:40 +0100

    x

--- test/mocks/ERC20Mintable.sol
+++ test/mocks/ERC20Mintable.sol
@@ -5,8 +5,10 @@ import "openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract ERC20Mintable is ERC20 {
     constructor(string memory name_, string memory symbol_)
-        ERC20(name_, symbol_, 18)
-    {}
+        ERC20(name_, symbol_)
+    {
+        
+    }
 
     function mint(uint256 amount, address to) public {
         _mint(to, amount);
