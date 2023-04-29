29653615d54c06e1302aca664483a4e2234cbc22
Lewis <lewis.broadhurst@mahimarkets.com>
2023-04-29 10:43:43 +0100

    x

--- src/V1/Exchange.sol
+++ src/V1/Exchange.sol
@@ -25,19 +25,33 @@ contract Exchange is ERC20 {
         token.transferFrom(msg.sender, address(this), _tokenAmount);
     }
 
-    
+    // Need to - msg.value from balance as the function has already taken the msg.balance from payable
+    function ethToTokenSwap(uint256 _minTokens) public payable {
+        uint256 tokenReserve = getTokenReserves();
+        uint256 tokensBought = getAmount(
+            msg.value,
+            address(this).balance - msg.value,
+            tokenReserve
+        );
+        require(tokensBought >= 0, "insufficient token output");
+
+        IERC20(tokenAddress).transfer(msg.sender, tokensBought);
+    }
+
+    function tokenToEthSwap(uint256 _tokensSold, uint256 _minEth) public payable {
+        uint256 tokenReserve = getTokenReserves();
+        uint256 ethBought = getAmount(
+            _tokensSold,
+            ethReserve,
+            address(this).balance
+        );
+        require(ethBought >= _minEth, "insufficient output amount");
+
+        IERC20(tokenAddress).transferFrom(msg.sender, address(this), _tokensSold);
+        payable(msg.sender).transfer(ethBought);
+    }
 
     // Helper Functions
-    function getReserve() public view returns (uint256) {
-        return IERC20(tokenAddress).balanceOf(address(this));
-    }
-
-    function getPrice(uint256 inputReserve, uint256 outputreserve)
-    public pure returns (uint256)
-    {
-        require(inputReserve > 0 && outputreserve > 0, "Reserves must be greater than 0");
-        return (inputReserve * 1000) / outputreserve;
-    }
 
     // Low level functions -- make it private, why?
     // Would also be pointless to be a public function
@@ -47,6 +61,36 @@ contract Exchange is ERC20 {
         uint256 outputReserve
     ) private pure returns (uint256) {
         require(inputReserve > 0 && outputReserve > 0, "Reserves must be greater than 0");
+
         return (inputAmount * outputReserve) / (inputReserve + inputAmount);
+    }
+
+    function getTokenReserves() public view returns (uint256) {
+        return IERC20(tokenAddress).balanceOf(address(this));
+    }
+
+    function getPrice(uint256 inputReserve, uint256 outputreserve)
+    public pure returns (uint256)
+    {
+        require(inputReserve > 0 && outputreserve > 0, "Reserves must be greater than 0");
+
+        return (inputReserve * 1000) / outputreserve;
+    }
+
+    // Will need these to display to FE and calculate outputs
+    function getTokenAmount(uint256 _ethSold) public view returns (uint256) {
+        require(_ethSold > 0, "ETH sold must be over 0");
+        uint256 tokenReserve = getTokenReserves();
+
+        return getAmount(_ethSold, address(this).balance, tokenReserve);
+    }
+
+    // Will need these to display to FE and calculate outputs
+    function getEthAmount(uint256 _tokenSold) public view returns (uint256) {
+        require(_tokenSold > 0, "Token sold must be over 0");
+
+        tokenReserve = getTokenReserves();
+
+        return getAmount(_tokenSold, tokenReserve, address(this).balance);
     }
 }
